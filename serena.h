04114b9bd6785d278c0498b6c02dcbb7b6a2a7d6
Yuumei <shjesna@protonmail.com>
2026-07-01 21:42:31 +0200

    alignup

--- serena.h
+++ serena.h
@@ -4,17 +4,30 @@
 #ifndef SERENA_HDR
 #define SERENA_HDR
 
-void zhyivannye();
+#include <stdint.h>
+#include <stddef.h>
+
+/// Conditionally upsizes [byte_length] to include padding in order to satisfy memory alignment requirements.
+/// This works for all types with alignment requirements up to the alignment of max_align_t which is typically [8 bytes].
+/// Use alignup_ex to specify what alignment to alignup from.
+size_t alignup(size_t byte_length);
+size_t alignup_ex(size_t byte_length, size_t alignment);
 
 #endif
 
 #ifdef SERENA_IMPL
 #undef SERENA_IMPL
 
-#include <stdio.h>
+#include <stdalign.h>
 
-void zhyivannye() {
-   printf("zhyivannye miratte\n");
+const size_t C_platform_alignment = alignof(max_align_t);
+
+size_t alignup(size_t byte_length) {
+   return (byte_length + C_platform_alignment - 1) & ~(C_platform_alignment - 1);
+}
+
+size_t alignup_ex(size_t byte_length, size_t alignment) {
+   return (byte_length + alignment - 1) & ~(alignment - 1);
 }
 
 #endif
